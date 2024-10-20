9ad5724ba26d5cadca1e835cb00e9ae8c595d539
fluffy persimmon i mighttt? <104640737+Nimakhazayi@users.noreply.github.com>
2024-10-20 11:31:40 +0330

    Add files via upload
    This version of the last code is the better one, which can encrypt a complete text file

---
+++ encrypting.py
@@ -0,0 +1,32 @@
+
+txt = str(input(" File address please: "))
+const = int(input(" The constant of a linear replacement you want in order of the English alphabet: "))
+coef = int(input(" The coeff of a linear replacement you want in order of the English alphabet: "))
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+cipher = []
+
+def encrypting(b):
+    file = open(f"{b}", "r")
+    content = file.read()
+    for i in content:
+        if i == " ":
+            cipher.append(" ")
+        elif i == "\n":
+            cipher.append("\n")
+        elif i == ".":
+            cipher.append(".")
+        elif i == "?":
+            cipher.append("?")
+        elif i == "!":
+            cipher.append("!")
+        elif i == ",":
+            cipher.append(",")
+        else:
+            j = alphabet.index(i)
+            index = ((j + 1)*coef + const) % 26
+            cipher.append(alphabet[index - 1])
+    encrypted = "".join(cipher)
+    print(f"\n{encrypted}")
+    file.close()
+
+encrypting(txt)
